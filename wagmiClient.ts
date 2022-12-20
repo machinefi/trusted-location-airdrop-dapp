042fc05ef71cfd68908c72c13534e80def2ab15f
GiupiDeLuca <giupi.deluca@hotmail.com>
2022-12-20 16:18:31 -0500

    updated wagmi client

--- wagmiClient.ts
+++ wagmiClient.ts
@@ -1,51 +1,36 @@
-import { createClient, configureChains, mainnet } from 'wagmi'
-import { publicProvider } from 'wagmi/providers/public'
+import { configureChains, createClient } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
-const { provider, webSocketProvider } = configureChains(
- [mainnet],
- [publicProvider()],
-)
+import {
+    iotexMainnetChain,
+    iotexTestnetChain,
+} from "./config/iotexChainConfig";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+    [iotexMainnetChain, iotexTestnetChain],
+    [
+        jsonRpcProvider({
+            rpc: (chain) => {
+                if (
+                    chain.id !== iotexMainnetChain.id &&
+                    chain.id !== iotexTestnetChain.id
+                )
+                    return null;
+                return { http: `${chain.rpcUrls.default}` }
+            },
+        }),
+        publicProvider(),
+    ]
+);
+
+const metamaskConnector = new MetaMaskConnector({
+    chains,
+});
 
 export default createClient({
- autoConnect: true,
- provider,
- webSocketProvider,
-})
-
-
-// import { configureChains, createClient } from "wagmi";
-// import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-
-// import {
-//   iotexMainnetChain,
-//   iotexTestnetChain,
-// } from "../../config/IotexChainConfig";
-// import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
-// import { publicProvider } from "wagmi/providers/public";
-
-// const { chains, provider } = configureChains(
-//   [iotexMainnetChain, iotexTestnetChain],
-//   [
-//     jsonRpcProvider({
-//       rpc: (chain) => {
-//         if (
-//           chain.id !== iotexMainnetChain.id &&
-//           chain.id !== iotexTestnetChain.id
-//         )
-//           return null;
-//         return { http: chain.rpcUrls.default };
-//       },
-//     }),
-//     publicProvider(),
-//   ]
-// );
-
-// const metamaskConnector = new MetaMaskConnector({
-//   chains,
-// });
-
-// export default createClient({
-//   autoConnect: true,
-//   connectors: [metamaskConnector],
-//   provider,
-// });
+    autoConnect: true,
+    connectors: [metamaskConnector],
+    provider,
+});
