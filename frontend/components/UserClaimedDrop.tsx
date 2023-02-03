d0a2b3b8412a7df39d8641823a5be88d8db805e9
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-02-03 17:31:01 +0100

    create useIsAirdropClaimed hook

--- frontend/components/UserClaimedDrop.tsx
+++ frontend/components/UserClaimedDrop.tsx
@@ -1,28 +1,17 @@
-import { useContractRead, useAccount } from "wagmi";
-import { LocationAirdrop } from "../config/contracts";
+import { useIsAirdropClaimed } from "../hooks/useIsAirdropClaimed";
 import AirdropCard from "./airdrop/AirdropCard";
 import { PersonalDropCardBody } from "./airdrop/PersonalDropCardBody";
 
 export const UserClaimedDrop = ({ hash }: { hash: string }) => {
-  const { address } = useAccount();
+  const isClaimed = useIsAirdropClaimed(hash);
 
-  const { data: isClaimed } = useContractRead({
-    ...LocationAirdrop,
-    functionName: "claimedAirDrops",
-    args: [address, hash],
-    enabled: !!address && !!hash,
-    onError(error) {
-      console.log("Error", error);
-    },
-  });
-
-  if (isClaimed) {
-    return (
-      <AirdropCard hash={hash}>
-        <PersonalDropCardBody hash={hash} />
-      </AirdropCard>
-    );
+  if (!isClaimed) {
+    return null;
   }
 
-  return null;
+  return (
+    <AirdropCard hash={hash}>
+      <PersonalDropCardBody hash={hash} />
+    </AirdropCard>
+  );
 };
