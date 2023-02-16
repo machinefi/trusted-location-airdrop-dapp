6e7496c276a882029c8c025fa9b05f4b65ea426f
GiupiDeLuca <giupi.deluca@hotmail.com>
2023-02-16 16:46:56 -0500

    clean contracts

--- web3/contracts/LocationAirdrop.sol
+++ web3/contracts/LocationAirdrop.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./LocationNFT.sol";
